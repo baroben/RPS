453adcdf960bcf592e3962b53555a80aa42eb8e5
ben <baronibenoit00@gmail.com>
2023-08-14 17:17:24 +0200

    Adds the scores

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const nGame = 5;
+var playerScore = 0;
+var computerScore = 0;
+
 function playRound(playerSelection,computerSelection){
-    if(playerSelection=="rock" && computerSelection=="scissor" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissor" && computerSelection=="paper")
+    if(playerSelection=="rock" && computerSelection=="scissor" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissor" && computerSelection=="paper"){
+        playerScore++;
         return `You WIN! ${playerSelection} beats ${computerSelection}`;
+    }
     else if(playerSelection == computerSelection)
         return `This is a draw! ${playerSelection} vs ${computerSelection}`
-    else
+    else{
+        computerScore++;
         return `You LOSE! ${computerSelection} beats ${playerSelection}`;
+    }
 }
 
 const choices = ["rock","paper","scissor"]; 
@@ -12,12 +20,9 @@ function getComputerChoice(){
     return choices[Math.floor(choices.length*Math.random())]
 }
 
-const nGame = 5;
-
-var playerSelection = undefined;
-var computerSelection = undefined;
-
 function game(){
+    var playerSelection = undefined;
+    var computerSelection = undefined;
     for(var i = 0; i<nGame ; i++){
         do{
             playerSelection = prompt("Make a choice : ").toLowerCase();
@@ -25,6 +30,7 @@ function game(){
         computerSelection = getComputerChoice();
         console.log(playRound(playerSelection,computerSelection));
     }
+    console.log(`Final score ! Player : ${playerScore} || Computer : ${computerScore}`);
 }
 
 game();
