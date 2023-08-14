0a03abbdc006e9430270742ccd6f53b2cc5c4557
ben <baronibenoit00@gmail.com>
2023-08-14 17:09:45 +0200

    Corrects typo on scissor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function playRound(playerSelection,computerSelection){
-    if(playerSelection=="rock" && computerSelection=="scisor" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scisor" && computerSelection=="paper")
+    if(playerSelection=="rock" && computerSelection=="scissor" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissor" && computerSelection=="paper")
         return `You WIN! ${playerSelection} beats ${computerSelection}`;
     else if(playerSelection == computerSelection)
         return `This is a draw! ${playerSelection} vs ${computerSelection}`
@@ -7,7 +7,7 @@ function playRound(playerSelection,computerSelection){
         return `You LOSE! ${computerSelection} beats ${playerSelection}`;
 }
 
-const choices = ["rock","paper","scisor"]; 
+const choices = ["rock","paper","scissor"]; 
 function getComputerChoice(){
     return choices[Math.floor(choices.length*Math.random())]
 }
